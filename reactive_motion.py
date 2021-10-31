50b1dc891b0e4ebd64986be6302c7ae83b0855ce
Zlata Sosnovitskaya <sosnozlat@yandex.ru>
2021-10-31 11:57:53 +0300

    Create reactive_motion.py
    График - траектория ракеты

---
+++ reactive_motion.py
@@ -0,0 +1,82 @@
+import math
+MODEL_G=9.8
+DT=0.001  
+
+# Сначала можно написать для любого тела движущегося под углом к горизонту (Класс "Тело")
+
+class Body:
+    def __init__(self,x,y,vx,vy):
+        '''
+        Парметры:
+        x,y - начальные координаты
+        vx,vy - начальная скорость
+        '''
+        self.x=x
+        self.y=y
+
+        self.vx=vx
+        self.vy=vy
+
+        self.trajectory_x = [] 
+        self.trajectory_y = []
+
+    def advance(self):  # Составление траектории тела, свободно движущегося под углом к горизонту
+
+        self.trajectory_x.append(self.x)
+        self.trajectory_y.append(self.y)
+
+        self.x+= self.vx*DT
+        self.y+= self.vy*DT -MODEL_G*(DT**2)/2
+        self.vy-=MODEL_G*DT
+    
+class Rocket(Body):
+    def __init__(self,m,dm,v,M):
+        '''
+        Параметры:
+        x,y-координаты в плоскости
+        M-масса ракеты
+        dm-масса газов, выбрасываемых за время DT
+        m-масса газа в ракете
+        v=скорость газов относительно ракеты
+        '''
+        self.m=m
+        self.v=v
+        self.dm=dm
+        super().__init__(0,0, 15, 20) # Свойства тела переносятся на ракету
+        self.alfa=math.atan(self.vy/self.vx) # Рассчитаем начальный угол полёта
+        self.a=v*dm/(DT*(M+m)) #Расчитаем ускорение, создаваемое реактивной силой
+        self.dm=dm
+
+    def advance(self):
+        super().advance()
+        while self.m>0:
+            self.x+=self.a*math.cos(self.alfa)*(DT**2)/2
+            self.y+=self.a*math.sin(self.alfa)*(DT**2)/2
+
+            self.m-=self.dm
+
+            self.vx+=self.a*math.cos(self.alfa)*DT
+            self.vy+=self.a*math.sin(self.alfa)*DT
+
+
+import numpy as np
+'''
+Зададим начальные параметры для тела и ракеты
+'''
+b = Body(0, 0, 15, 20)
+r = Rocket(10,5,10,10)
+
+bodies = [b, r]
+
+'''
+Теперь построим графики движения ракеты и тела в одной системе коррдинат для удобного сравнения
+'''
+for t in np.arange(0, 4, DT): 
+    for b in bodies: 
+        b.advance() 
+
+from matplotlib import pyplot as pp
+
+for b in bodies: 
+    pp.plot(b.trajectory_x, b.trajectory_y)
+pp.show()
