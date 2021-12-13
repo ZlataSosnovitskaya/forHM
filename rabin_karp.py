3c764cc47bd739ec3434cefe8ee5d99b9ea83406
Zlata Sosnovitskaya <sosnozlat@yandex.ru>
2021-12-14 00:54:27 +0300

    Update rabin_karp.py

--- rabin_karp.py
+++ rabin_karp.py
@@ -1,7 +1,4 @@
-'''
-Алгоритм Рабина-Карпа для поиска вхождений 
-'''
-def find_all_rabin_karp(text, pattern):
+def find_all_rabin_karp(text, pattern): #  Алгоритм Рабина-Карпа для поиска вхождений образца в текст
     result = []
     patternsum = sum(ord(s) for s in pattern)
     textwindowsum = sum(ord(text[j]) for j in range(len(pattern)))
@@ -11,6 +8,5 @@ def find_all_rabin_karp(text, pattern):
             if text.startswith(pattern, i):
                 result.append(i)
         textwindowsum = textwindowsum - ord(text[i])+ord(text[i+len(pattern)])      
-    return result        
-
-print(find_all_rabin_karp('abcabcabcabcabdhdpshabc', 'abc'))
+    return result 
+      
