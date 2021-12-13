a998622b8b8f2c0bf1fb107520b0dea604f4f63b
Zlata Sosnovitskaya <sosnozlat@yandex.ru>
2021-12-14 00:51:28 +0300

    Create rabin_karp.py

---
+++ rabin_karp.py
@@ -0,0 +1,16 @@
+'''
+Алгоритм Рабина-Карпа для поиска вхождений 
+'''
+def find_all_rabin_karp(text, pattern):
+    result = []
+    patternsum = sum(ord(s) for s in pattern)
+    textwindowsum = sum(ord(text[j]) for j in range(len(pattern)))
+    text = text + ' '
+    for i in range(len(text) - len(pattern)):     
+        if patternsum == textwindowsum:
+            if text.startswith(pattern, i):
+                result.append(i)
+        textwindowsum = textwindowsum - ord(text[i])+ord(text[i+len(pattern)])      
+    return result        
+
+print(find_all_rabin_karp('abcabcabcabcabdhdpshabc', 'abc'))
